4b2b1b9952335745f85d5746113119dd1321b9bd
tkht260310 <266846337+tkht260310@users.noreply.github.com>
2026-08-14 22:45:06 +0900

    Rails Tutorial 第1章：1.3.4 演習（¡Hola, mundo!／goodbye アクション追加）

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "hello, world!"
+    render html: "¡Hola, mundo!"
+  end
+  def goodbye
+    render html: "goodbye!, world"
   end
 end
